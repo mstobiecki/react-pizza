87e980acdb8d1f7cece41121e709885705582feb
Mateusz Stobiecki <m100biecki@gmail.com>
2023-09-03 17:31:15 +0200

    added logic for navigation

--- src/components/Layout/Header/Navigation.js
+++ src/components/Layout/Header/Navigation.js
@@ -1,30 +1,52 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import classes from './Navigation.module.css';
 import { List } from '@phosphor-icons/react';
 
 const Navigation = () => {
+	const [navigationOpen, setNavigationOpen] = useState(false);
+
+	const navigationHandler = () => {
+		setNavigationOpen((prevState) => !prevState);
+	};
+
+	const navigationClasses = navigationOpen
+		? `${classes.navigation} ${classes['navigation-open']}`
+		: `${classes.navigation}`;
+
 	return (
 		<Fragment>
-			<nav className={`${classes.navigation} ${classes['navigation-pen']}`}>
+			<nav className={navigationClasses}>
 				<ul className={classes['navigation-list']}>
 					<li className={classes['navigation-item']}>
-						<a href='#' className={classes['navigation-link']}>
+						<a
+							href='#'
+							onClick={navigationHandler}
+							className={classes['navigation-link']}>
 							O nas
 						</a>
 					</li>
 					<li className={classes['navigation-item']}>
-						<a href='#' className={classes['navigation-link']}>
+						<a
+							href='#'
+							onClick={navigationHandler}
+							className={classes['navigation-link']}>
 							Menu
 						</a>
 					</li>
 					<li className={classes['navigation-item']}>
-						<a href='#' className={classes['navigation-link']}>
+						<a
+							href='#'
+							onClick={navigationHandler}
+							className={classes['navigation-link']}>
 							Kontakt
 						</a>
 					</li>
 				</ul>
 			</nav>
-			<button className={classes['navigation-control']} type='button'>
+			<button
+				onClick={navigationHandler}
+				className={classes['navigation-control']}
+				type='button'>
 				<List className={classes['navigation-button']} />
 			</button>
 		</Fragment>
