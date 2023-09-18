6c9dc1dd722237d876a12567141c4d8fe801a675
Mateusz Stobiecki <m100biecki@gmail.com>
2023-09-18 21:34:32 +0200

    added support for all devices for Offer.js with History.js

--- src/components/Layout/Gastronomy/Gastronomy.js
+++ src/components/Layout/Gastronomy/Gastronomy.js
@@ -13,16 +13,18 @@ const Gastronomy = () => {
 			<Card>
 				<div className={classes.wrapper}>
 					<div className={classes.box}>
-						<h2 className={classes.title}>Składniki najwyższej jakości</h2>
-						<p className={classes.description}>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-							enim ad minim veniam, quis nostrud exercitation ullamco laboris
-							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-							reprehenderit in voluptate velit esse cillum dolore eu fugiat
-							nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-							sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
+						<div className={classes['text-box']}>
+							<h2 className={classes.title}>Składniki najwyższej jakości</h2>
+							<p className={classes.description}>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+								enim ad minim veniam, quis nostrud exercitation ullamco laboris
+								nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+								in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+								nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+								sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+						</div>
 						<div className={classes['image-box']}>
 							<img
 								className={classes.image}
@@ -32,16 +34,18 @@ const Gastronomy = () => {
 						</div>
 					</div>
 					<div className={classes.box}>
-						<h2 className={classes.title}>Przepisy tworzone z miłości</h2>
-						<p className={classes.description}>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-							enim ad minim veniam, quis nostrud exercitation ullamco laboris
-							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-							reprehenderit in voluptate velit esse cillum dolore eu fugiat
-							nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-							sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
+						<div className={classes['text-box']}>
+							<h2 className={classes.title}>Przepisy tworzone z miłości</h2>
+							<p className={classes.description}>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+								enim ad minim veniam, quis nostrud exercitation ullamco laboris
+								nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+								in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+								nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+								sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+						</div>
 						<div className={classes['image-box']}>
 							<img
 								className={classes.image}
